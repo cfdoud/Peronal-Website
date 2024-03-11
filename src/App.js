4ff6fb6fb618508fe3cf8f9b182e475e78dfaaa5
cfdoud <prouddoud01@gmail.com>
2024-03-11 13:51:43 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='container'>
+          <h1 className='display-3'>Christian Doud</h1>
+          <h2 className='lead'>Software Developer</h2>
+        </div>
       </header>
     </div>
   );
