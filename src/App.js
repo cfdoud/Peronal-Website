c54c0a19d6eb80f910d177fe654ca1c47b8ea86e
cfdoud <prouddoud01@gmail.com>
2024-08-24 17:45:32 -0700

    Django

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import './App.css';
 //import { textElement} from 'react';
-import ZSE from './assets/ZSE.png';
 import Navbar from './components/header/header';
 import Bio from './components/bio/Bio';
 import Toolbox from './components/toolbox-block/tool-box';
+import Project from './components/Projects-box/projects';
+
 function App() {
-  //const textRef = useRef(null);
 
-  // const handleClick = () => {
-  //   console.log('Button clicked');
-    
-  // };
 
-  
-
-  
 
   return (
     
     <div className="Background">
-      <div className="App">
-        
+      <div className="App">   
         <Navbar />
-
         <Bio />
-      
-        
-
   </div>
 
 
@@ -43,40 +30,7 @@ function App() {
   <Toolbox />
   
   
-  {/* Projects Section */}
-  <div className='p-5 parent-container'>
-    <div className='title-container'>
-      <h3 className='tool-text'>Recent Projects</h3>
-    </div>
-  </div>
-
-  <div className='project-container'>
-    <div className='project-card'>
-      <div className='project-image'>
-        <div className='project-text'>
-          <a href='https://github.com/cfdoud/Zombie-Shooter-Extreme' style={{textDecoration: 'none', color: 'green'}}>Zombie Shooter Extreme!</a>
-          <p>Zombie style shooter game created with Raylib library.</p>
-        </div>
-        <a href='https://github.com/cfdoud/Zombie-Shooter-Extreme'>
-          <img src={ZSE} className='project-image' alt=' goes here' />
-        </a>
-      </div>
-    </div>
-
-
-    <div className='project-card'>
-      <div className='project-image'>
-        <div className='project-text'>
-          <a href='https://github.com/cfdoud/Finsta-gram' style={{textDecoration: 'none', color: 'green'}}>Finstagram</a>
-            <p>Instagram-type site where users can dynamically share and view their best moments.</p>
-        </div>
-        <a href='https://github.com/cfdoud/Finsta-gram'>
-          <img src={ZSE} className='project-image' alt=' goes here' />
-        </a>      
-      </div>
-    </div>
-  
-  </div>        
+   <Project />  
   
 
 
---
+++ src/components/Projects-box/projects.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import './projects.css';
+
+function Project() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        // Fetch data from Django backend
+        fetch('http://127.0.0.1:8000/api/projects/')
+            .then(response => response.json())
+            .then(data => setProjects(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
+        <div>
+            <div className='p-5 parent-container'>
+                <div className='title-container'>
+                    <h3 className='tool-text'>Recent Projects</h3>
+                </div>
+            </div>
+
+            <div className='project-container'>
+                {projects.map(project => (
+                    <div className='project-card' key={project.id}>
+                        <div className='project-image'>
+                            <div className='project-text'>
+                                <a href={project.url} style={{ textDecoration: 'none', color: 'green' }}>
+                                    {project.name}
+                                </a>
+                                <p>{project.description}</p>
+                            </div>
+                            <a href={project.url}>
+                                <img src={project.image} className='project-image' alt={project.name} />
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Project;
